87f663dcf4019f467384ab6f8c5edcfa0c5ff5e0
GarySimons <garycsimons@gmail.com>
2021-02-09 07:37:42 +0000

    Playing with Python

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-name = input("What's your name? ")
+number = int(input("Please enter a number:"))
 
-age = input("What's your age: ")
-
-print(f"Hello {name}, you are {age} years old")
+if number > 5:
+    print(f"{number} is greater than 5")
+elif number < 5:
+    print(f"{number} is less than 5")
+else:
+    print(f"{number} is not greater than, or less than 5. Therefore, {number} must be equal to 5.")
