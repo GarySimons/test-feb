eeddd9c1a561e48d33f6bdf27ac06a7f711f4ae3
GarySimons <garycsimons@gmail.com>
2021-02-10 06:43:45 +0000

    Python loops

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-languages = ["HTML", "CSS", "JavaScript"]
-for language in languages:
-    print(language)
+fruits = ["apple", "banana", "peach", "pear", "plum", "orange"]
+counter = 0
 
-for character in "Python":
-    print(character)
+while counter < len(fruits):
+    if fruits[counter] == "banana":
+        counter += 1
+        continue
+
+    print(fruits[counter])
+    counter += 1
+
+print("Iteration completed")
