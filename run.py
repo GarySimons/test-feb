b86debdc460661906d8a4401866bfd55e16225e2
GarySimons <garycsimons@gmail.com>
2021-02-11 07:41:28 +0000

    Python tuples

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
-fruits = ["apple", "banana", "peach", "pear", "plum", "orange"]
-print(fruits[::-1])
+user = ("tombom", "Tom", "Bom")
+
+print("Username: %s" % user[0])
+print("First Name: %s" % user[1])
+print("Last Name: %s" % user[2])
