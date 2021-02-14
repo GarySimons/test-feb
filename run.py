71ead37b7c4c9d018c6d4177c0f5a8be07f2375a
GarySimons <garycsimons@gmail.com>
2021-02-14 15:10:06 +0000

    Python tests

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
-from itertools import product
+"""
+10 min
+Implement the unique_names method. When passed two arrays of names, it will
+return an array containing the names that appear in either or both arrays.
+The returned array should have no duplicates.
+For example, calling unique_names(['Ava', 'Emma', 'Olivia'], ['Olivia', 'Sophia', 'Emma'])
+should return an array containing Ava, Emma, Olivia, and Sophia in any order.
+    
+"""
+from itertools import chain
 
-
-class IceCreamMachine:
-
-    def __init__(self, ingredients, toppings):
-        self.ingredients = ingredients
-        self.toppings = toppings
-
-    def scoops(self):
-        return list(product(self.ingredients, self.toppings))
-
-
-machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
-print(machine.scoops())  # should print[['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
+def unique_names(names1, names2):
+    return list(set(chain(names1, names2)))
+        
+names1 = ["Ava", "Emma", "Olivia"]
+names2 = ["Olivia", "Sophia", "Emma"]
+print(unique_names(names1, names2)) # should print Ava, Emma, Olivia, Sophia
